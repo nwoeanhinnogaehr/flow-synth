ed286575f7ea8fa9216f22002069c3a0dfcbdb63
Noah Weninger <nweninge@ualberta.ca>
2017-10-09 19:08:59 -0400

    comment about dlopen hack

--- src/plugin_loader.rs
+++ src/plugin_loader.rs
@@ -12,6 +12,13 @@ pub struct NodeLibrary {
 }
 
 impl NodeLibrary {
+    // path/file_path hack is because we need to force dlopen to reload
+    // so path gets copied to file_path before loading,
+    // then we actually load file_path,
+    // and delete it afterwards.
+    //
+    // reason it isn't being unloaded seems to be because pthread also dlopens anything
+    // we dlopen that starts a thread, then doesn't dlclose as quickly as we would like it to.
     pub fn load(path: &str, file_path: &str) -> Result<NodeLibrary> {
         let lib = Library::new(file_path)?;
         unsafe {
